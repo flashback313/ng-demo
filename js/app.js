0c6b3e912607196c83a43b2264e5a3c270fd4814
flashback313 <windmark2012@gmail.com>
2016-01-16 11:59:44 +0800

    test directive parent child data share

--- js/app.js
+++ js/app.js
@@ -4,8 +4,29 @@ angular.module('myapp', [])
 	.directive('hello', [function() {
 		return {
 			restrict: 'E',
-			template: '<span>Hello Angular</span>',
-			link: function(scope) {
+			transclude: true,
+			template: '<div>Hello<angular></angular></div>',
+			controller: ["$scope",function(scope) {
+				console.log(scope,this);
+				this.func = function() {
+					return "aaa";
+				}
+			}],
+			link: function(scope){
+				scope.aaa = "aaa";
+				scope.console = function() {
+					console.log("get parent scope ok");
+				}
+			}
+		};
+	}])
+	.directive('angular', [function() {
+		return {
+			restrict: 'EA',
+			transclude: true,
+			template: '<button ng-click="console()">Angular</button>',
+			require: "?hello",
+			link: function(scope,element,attrs,helloCtrl) {
 				console.log(scope);
 			}
 		};
